d3fd38c9fc157e67898e8dc49da7a5be8f3ca557
DurgeshPatil13 <durgeshpatil0513@gmail.com>
2026-06-07 21:53:17 +0530

    completer

--- js/main.js
+++ js/main.js
@@ -9,3 +9,29 @@ hamburger.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     navside.classList.remove("active");
 });
+const navLinks = document.querySelectorAll(".navside a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navside.classList.remove("active");
+    });
+});
+const workbtn=document.querySelector(".work");
+const cvbtn=document.querySelector(".cvbtn");
+workbtn.addEventListener("click",()=>{
+        document.getElementById("worksection").scrollIntoView({
+        behavior: "smooth"
+    });
+})
+const div = document.querySelector(".talk-btn");
+const icon = div.querySelector("i");
+
+div.addEventListener("click", () => {
+    icon.classList.toggle("fa-moon");
+    icon.classList.toggle("fa-sun");
+});
+
+
+div.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
